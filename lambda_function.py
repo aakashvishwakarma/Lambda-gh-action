3c82d99dcec32b32b8c5c271b97fe6113a8fe8dd
aakashvishwakarma <50501190+aakashvishwakarma@users.noreply.github.com>
2021-09-07 16:51:05 +0530

    Add files via upload

---
+++ lambda_function.py
@@ -0,0 +1,13 @@
+import json
+import requests
+import pytz
+
+
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+        
+    }
